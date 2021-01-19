7800ca0c7eb2db8823b522d678cbd56638816ddf
TrongDungEmb98 <ntdungembedded98@gmail.com>
2021-01-19 18:53:02 +0700

    fix blynk bug

--- Arduino_Enthernet/src/main.cpp
+++ Arduino_Enthernet/src/main.cpp
@@ -1,26 +1,24 @@
 #include <Arduino.h>
+
+
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <BlynkSimpleEthernet.h>
 #include "DHT.h"
 
-#define RELAY1_PIN 12
-#define RELAY2_PIN 13
+#define RELAY1_PIN 11
+#define RELAY2_PIN 12   
 
-#define BUTTON1_PIN 8
-#define BUTTON2_PIN 9
+#define BUTTON1_PIN 6
+#define BUTTON2_PIN 7
 
-#define LM35_PIN A5
-#define DHT11_PIN 2
+#define DHT11_PIN       2
 #define DHTTYPE DHT11
 
-#define W5100_CS 10
-#define SDCARD_CS 4
+#define W5100_CS        10
 
-char auth[] = "55yMmZjN1j3PMEWfvq_5Y-bJZkR5qHCS";
-
-float lm35_temperature = 0;
-float lm35_temperature_threshold = 0;
+char auth[] = "5SsX9k6e7WrNcSEXx6gxIpHexKH5tRSJ";
 
 float dht11_humidity = 0;
 float dht11_humidity_threshold = 0;
@@ -32,11 +30,9 @@ static uint8_t relay2_state = 0;
 static uint8_t btn1State = HIGH;
 static uint8_t btn2State = HIGH;
 
-#define lm35_virtual_pin V1
-#define lm35_virtual_threshold_pin V2
-#define dht11_huminity_virtual_pin V3
+#define dht11_huminity_virtual_pin V5
 #define dht11_huminity_threshold_virtual_pin V4
-#define dht11_temperature_virtual_pin V5
+#define dht11_temperature_virtual_pin V3
 #define dht11_temperature_threshold_virtual_pin V6
 
 #define relay1_virtual_pin V7
@@ -45,9 +41,7 @@ static uint8_t btn2State = HIGH;
 DHT dht(DHT11_PIN, DHTTYPE);
 BlynkTimer timer;
 
-void lm35_read();
 void dht11_read();
-void check_threshold();
 void checkPhysicalButton();
 
 void setup()
@@ -60,24 +54,20 @@ void setup()
     pinMode(BUTTON1_PIN, INPUT_PULLUP);
     pinMode(BUTTON2_PIN, INPUT_PULLUP);
 
-    pinMode(SDCARD_CS, OUTPUT);
-    digitalWrite(SDCARD_CS, HIGH);
-
     dht.begin();
 
     Blynk.begin(auth);
     timer.setInterval(1000L, dht11_read);
-    timer.setInterval(1000L, lm35_read);
-    timer.setInterval(1000L, check_threshold);
     timer.setInterval(100L, checkPhysicalButton);
 }
 
 void loop()
 {
     Blynk.run();
-    timer.run();
+    // timer.run();
+    dht11_read();
+    checkPhysicalButton();
 }
-
 void checkPhysicalButton()
 {
     if (digitalRead(BUTTON1_PIN) == LOW)
@@ -110,10 +100,10 @@ void checkPhysicalButton()
 
             // Toggle LED state
             relay2_state = !relay2_state;
-            digitalWrite(RELAY1_PIN, relay2_state);
+            digitalWrite(RELAY2_PIN, relay2_state);
 
             // Update Button Widget
-            Blynk.virtualWrite(relay1_virtual_pin, relay2_state);
+            Blynk.virtualWrite(relay2_virtual_pin, relay2_state);
         }
         btn2State = LOW;
     }
@@ -137,17 +127,6 @@ void dht11_read()
     Blynk.virtualWrite(dht11_temperature_virtual_pin, dht11_temperature);
 }
 
-void lm35_read()
-{
-    int lm35_reader = analogRead(LM35_PIN);
-    float voltage = lm35_reader * 5.0 / 1024.0;
-    lm35_temperature = voltage * 100.0;
-
-    Blynk.virtualWrite(lm35_virtual_pin, lm35_temperature);
-    // Serial.print("LM35 nhiet do: ");
-    // Serial.println(lm35_temperature);
-}
-
 void control_relay(uint8_t relay, uint8_t value)
 {
     switch (relay)
@@ -169,37 +148,6 @@ void control_relay(uint8_t relay, uint8_t value)
     default:
         break;
     }
-}
-
-void check_threshold()
-{
-
-    if (lm35_temperature_threshold != 0 &&
-        lm35_temperature >= lm35_temperature_threshold)
-    {
-        control_relay(1, LOW);
-    }
-    else if (lm35_temperature_threshold != 0 &&
-             lm35_temperature < lm35_temperature_threshold)
-    {
-        control_relay(1, HIGH);
-    }
-
-    if (dht11_humidity_threshold != 0 &&
-        dht11_humidity_threshold > dht11_humidity)
-    {
-        control_relay(2, LOW);
-    }
-    else if (dht11_humidity_threshold != 0 &&
-             dht11_humidity_threshold < lm35_temperature_threshold)
-    {
-        control_relay(2, HIGH);
-    }
-}
-
-BLYNK_WRITE(lm35_threshold_virtual_pin)
-{
-    lm35_temperature_threshold = param.asInt();
 }
 
 BLYNK_WRITE(dht11_huminity_threshold_virtual_pin)
@@ -238,7 +186,7 @@ BLYNK_WRITE(relay2_virtual_pin)
     Serial.print("relay2_virtual_pin: ");
     Serial.println(relay2_state);
 
-    if (relay1_state == 0)
+    if (relay2_state == 0)
     {
         Serial.println("relay2_virtual_pin: LOW");
         digitalWrite(RELAY2_PIN, LOW);
