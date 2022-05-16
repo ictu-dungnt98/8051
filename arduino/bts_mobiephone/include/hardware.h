7709c3253f1a7d19eeabcc2ff0a0330326dccfb0
TrongDungEmb98 <ntdungembedded98@gmail.com>
2022-05-16 23:54:58 +0700

    add BTS

---
+++ arduino/bts_mobiephone/include/hardware.h
@@ -0,0 +1,23 @@
+#ifndef HARDWARE_H_
+#define HARDWARE_H_
+
+#include <Arduino.h>
+
+#define SDA_PIN D4
+#define SCL_PIN D5
+
+#define ADC_PIN	A0
+
+#define IR_RE 	D2
+#define IR_TR1 	D16
+#define IR_TR1 	D14
+
+#define RELAY1	D15
+#define RELAY2	D3
+
+#define HC595_DATA_PIN	D0 /* 74HC595 DS pin */
+#define HC595_CLK_PIN	D12 /* clock pin */
+#define HC595_LATCH_PIN	D13 /* latch pin */
+
+
+#endif
