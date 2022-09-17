d072561c32449b337a20d4def049116412f1e1e7
TrongDungEmb98 <ntdungembedded98@gmail.com>
2022-09-17 10:16:18 +0700

    add Hai's project

---
+++ arduino/presence_of_human_detector/include/rtc.h
@@ -0,0 +1,13 @@
+#ifndef _RTC_H_
+#define _RTC_H_
+
+#include <Arduino.h>
+#include <time.h>
+
+#define MAX_CMD_ALARM      6
+
+void rtc_init(void);
+void rtc_hander(void);
+void alarm_loop(void);
+
+#endif
---
+++ arduino/presence_of_human_detector/include/sensor.h
@@ -0,0 +1,9 @@
+#ifndef _SENSOR_H_
+#define _SENSOR_H_
+
+#include <Arduino.h>
+
+void sensor_init();
+void sensor_read();
+
+#endif /* _SENSOR_H_ */
